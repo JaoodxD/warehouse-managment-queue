29c9557d8976e7662d76f54a6c732fce2a7bc69f
JaoodxD <jaood96@gmail.com>
2023-03-07 11:54:53 +0200

    add test case

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
-import { warehouse, statusHandler, orderRepository } from './services';
-import orderRepositoryService from './services/orderRepository';
+import services from './services/index.js';
 
+import { Bus } from './infra/bus/index.js';
 
+const bus = new Bus();
+for (const [name, ServiceConstructor] of Object.entries(services)) {
+  bus.registerService(name, ServiceConstructor);
+}
+
+const order = bus.command('orderRepository.createOrder', {
+  orderedItems: [
+    {
+      name: 'Coca-Cola',
+      amount: 1
+    },
+    {
+      name: 'Hamburger',
+      amount: 2
+    }
+  ]
+});
+
+console.log({ order });
